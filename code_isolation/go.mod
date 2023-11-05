e27ab5290dd940371110fa5fa6feab2ea85333eb
Johannes <jonny.bliha@gmail.com>
2023-11-05 13:49:45 +0100

    Add code isolation prototype

---
+++ code_isolation/go.mod
@@ -0,0 +1,5 @@
+module code_isolation
+
+go 1.21.3
+
+require github.com/wasmerio/wasmer-go v1.0.4 // indirect
---
+++ code_isolation/user.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+    sum(3,4)
+}
+
+func sum(a, b int) int {
+    return a + b
+}
