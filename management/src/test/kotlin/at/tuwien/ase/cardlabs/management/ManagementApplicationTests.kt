7fc910dbd0fbdeb6224aa0d008f5e4e39edc6683
Yekllurt <Yekllurt@users.noreply.github.com>
2023-11-06 16:39:10 +0100

    [7] migrate management from Java to Kotlin

---
+++ management/src/test/kotlin/at/tuwien/ase/cardlabs/management/ManagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package at.tuwien.ase.cardlabs.management
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ManagementApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
