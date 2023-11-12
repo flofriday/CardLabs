e424fe497537ab7f960a62ddf2a42eb16f3d6f38
UnHold (Alex) <alex@hold.re>
2023-11-12 16:13:44 +0100

    [#25] - made navbar smaller

--- frontend/app/components/NavBar/Profile.tsx
+++ frontend/app/components/NavBar/Profile.tsx
@@ -1,6 +1,6 @@
 export default function Profile() {
   return (
-    <div className="w-14">
+    <div className="w-11">
       <img
         src="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-2-800x800.jpg"
         alt="Profile Image"
--- frontend/app/components/NavBar/SignInUp.tsx
+++ frontend/app/components/NavBar/SignInUp.tsx
@@ -1,10 +1,10 @@
 export default function SignInUp() {
   return (
     <div className="flex space-x-6">
-      <button className="btn bg-secondary hover:bg-accent text-text py-4 px-8 rounded-full shadow-md text-xl">
+      <button className="btn bg-secondary hover:bg-accent text-text py-2 px-6 rounded-full shadow-md text-lg">
         Register
       </button>
-      <button className="btn bg-secondary hover:bg-accent text-text py-4 px-8 rounded-full shadow-md text-xl">
+      <button className="btn bg-secondary hover:bg-accent text-text py-2 px-6 rounded-full shadow-md text-lg">
         Login
       </button>
     </div>
