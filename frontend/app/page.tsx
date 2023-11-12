851ca5fd6536a9bc335725c35200549ac2a7128f
UnHold (Alex) <alex@hold.re>
2023-11-12 14:15:01 +0100

    [#25] - added landing page content

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,17 @@
 import Image from "next/image";
-
 export default function Home() {
-  return <main className="">Main Page</main>;
+  return (
+    <main className="flex justify-around">
+      <div className="text-text text-[7vw] font-base w-4/12 pt-[15vh]">
+        Elevate
+        <br />
+        your
+        <br />
+        strategies
+      </div>
+      <div className="w-5/12 pt-[5vh]">
+        <img src="/landingPageImg.svg" className="w-full" />
+      </div>
+    </main>
+  );
 }
