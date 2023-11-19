03bafe8fbf8b1f91887d93f7c938c20efdfad354
UnHold (Alex) <alex@hold.re>
2023-11-19 14:30:57 +0100

    [#25] - fixed linting error

--- frontend/app/components/NavBar/SignInUp.tsx
+++ frontend/app/components/NavBar/SignInUp.tsx
@@ -1,4 +1,4 @@
-export default function SignInUp() {
+export default function SignInUp(): JSX.Element {
   return (
     <div className="flex space-x-6">
       <button className="btn bg-secondary hover:bg-accent text-text py-2 px-6 rounded-full shadow-md text-lg">
