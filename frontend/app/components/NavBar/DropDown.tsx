7de47e9d626c50111430dcb983795e9e5cce69fd
UnHold (Alex) <alex@hold.re>
2023-11-19 23:22:29 +0100

    [#11] - added logout button + logout function

--- frontend/app/components/NavBar/DropDown.tsx
+++ frontend/app/components/NavBar/DropDown.tsx
@@ -1,13 +1,28 @@
+import { deleteCookie } from "cookies-next";
+import { useRouter } from "next/navigation";
+
 interface Props {
   className?: string;
 }
 
+function onLogout(router: any): void {
+  deleteCookie("auth_token");
+  router.refresh();
+  router.replace("/");
+}
+
 export default function DropDown({ className = "" }: Props): JSX.Element {
+  const router = useRouter();
   return (
     <div
       className={`fixed right-8 top-14 bg-primary w-56 p-3 rounded-md border-2 ${className}`}
     >
-      <button className="btn bg-accent text-text py-2 w-48 rounded-lg shadow-md text-lg">
+      <button
+        className="btn bg-accent text-text py-2 w-48 rounded-lg shadow-md text-lg"
+        onClick={() => {
+          onLogout(router);
+        }}
+      >
         Logout
       </button>
     </div>
