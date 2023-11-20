48794e56c3a9545be271f2c600a628ee8d07bc59
UnHold (Alex) <alex@hold.re>
2023-11-20 09:50:58 +0100

    [#11] - fixed notification type error

--- frontend/app/components/Notifications.tsx
+++ frontend/app/components/Notifications.tsx
@@ -18,15 +18,18 @@ const progressBar = {
   default: "bg-primary",
 };
 
+type background_ = keyof typeof background;
+type progressBar_ = keyof typeof progressBar;
+
 export default function Notifications(): JSX.Element {
   return (
     <ToastContainer
-      toastClassName={({ type }) =>
+      toastClassName={({ type }: { type: background_ }) =>
         // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
         background[type || "default"] +
         " relative flex p-1 min-h-10 rounded-md justify-between overflow-hidden cursor-pointer"
       }
-      progressClassName={({ type }) =>
+      progressClassName={({ type }: { type: progressBar_ }) =>
         // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
         progressBar[type || "default"] +
         " Toastify__progress-bar Toastify__progress-bar--animated"
