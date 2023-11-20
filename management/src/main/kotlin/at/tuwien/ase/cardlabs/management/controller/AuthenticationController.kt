ccff5ed53d4bdb94e6beb902500d5f49ddcd909e
Yekllurt <Yekllurt@users.noreply.github.com>
2023-11-20 17:41:06 +0100

    [11] improve login error handling

--- management/src/main/kotlin/at/tuwien/ase/cardlabs/management/controller/AuthenticationController.kt
+++ management/src/main/kotlin/at/tuwien/ase/cardlabs/management/controller/AuthenticationController.kt
@@ -3,7 +3,10 @@ package at.tuwien.ase.cardlabs.management.controller
 import at.tuwien.ase.cardlabs.management.security.JwtHelper
 import at.tuwien.ase.cardlabs.management.security.authentication.JwtAuthenticationResponse
 import at.tuwien.ase.cardlabs.management.security.authentication.LoginRequest
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -13,15 +16,22 @@ import org.springframework.web.bind.annotation.RestController
 class AuthenticationController(val authenticationManager: AuthenticationManager) {
 
     @PostMapping("/authentication/login")
-    fun login(@RequestBody loginRequest: LoginRequest): JwtAuthenticationResponse {
-        val authentication = authenticationManager.authenticate(
-            UsernamePasswordAuthenticationToken.unauthenticated(
-                loginRequest.username,
-                loginRequest.password
+    fun login(@RequestBody loginRequest: LoginRequest): ResponseEntity<JwtAuthenticationResponse> {
+        return try {
+            val authentication = authenticationManager.authenticate(
+                UsernamePasswordAuthenticationToken.unauthenticated(
+                    loginRequest.username,
+                    loginRequest.password
+                )
             )
-        )
 
-        val jwt = JwtHelper.generateToken(authentication)
-        return JwtAuthenticationResponse(loginRequest.username, jwt)
+            val jwt = JwtHelper.generateToken(authentication)
+            ResponseEntity.status(HttpStatus.OK)
+                .body(JwtAuthenticationResponse(loginRequest.username, jwt))
+        } catch (exception: BadCredentialsException) {
+            ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .build()
+        }
     }
 }
