ada38249dba7ba0b0ed0d77eb03dd3d51a8d3e6f
UnHold (Alex) <alex@hold.re>
2023-11-20 09:59:32 +0100

    [#11] - fixed type error again

--- frontend/app/components/Notifications.tsx
+++ frontend/app/components/Notifications.tsx
@@ -24,14 +24,14 @@ type progressBar_ = keyof typeof progressBar;
 export default function Notifications(): JSX.Element {
   return (
     <ToastContainer
-      toastClassName={({ type }: { type: background_ }) =>
+      toastClassName={({ type }: any) =>
         // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-        background[type || "default"] +
+        background[(type as background_) || "default"] +
         " relative flex p-1 min-h-10 rounded-md justify-between overflow-hidden cursor-pointer"
       }
-      progressClassName={({ type }: { type: progressBar_ }) =>
+      progressClassName={({ type }: any) =>
         // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-        progressBar[type || "default"] +
+        progressBar[(type as progressBar_) || "default"] +
         " Toastify__progress-bar Toastify__progress-bar--animated"
       }
       bodyClassName={() => "text-sm text-primary font-med block p-3"}
