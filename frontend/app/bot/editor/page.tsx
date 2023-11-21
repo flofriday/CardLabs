e7f6c43cf3346d7bf6ec7cde6a10b3fe3f3988c0
UnHold (Alex) <alex@hold.re>
2023-11-21 23:43:33 +0100

    [#17] - added basic file structure

---
+++ frontend/app/bot/editor/page.tsx
@@ -0,0 +1,9 @@
+import LeftPageHeader from "@/app/components/leftPageHeader";
+
+export default function BotEditor(): JSX.Element {
+  return (
+    <div className="w-full h-full">
+      <LeftPageHeader title="Adath Wacoffin" subTitle="Bot-Name" />
+    </div>
+  );
+}
---
+++ frontend/app/components/leftPageHeader.tsx
@@ -0,0 +1,20 @@
+interface Props {
+  title: string;
+  subTitle?: string;
+}
+
+export default function LeftPageHeader({
+  title,
+  subTitle = "",
+}: Props): JSX.Element {
+  return (
+    <div>
+      <h1 className="absolute text-6xl md:text-8xl mt-10 md:mt-16 ml-10 md:ml-16 font-medium tracking-wider inline-block w-fit">
+        {title}
+      </h1>
+      <h4 className="absolute text-l md:text-2xl mt-4 md:mt-8 ml-10 md:ml-16 font-medium tracking-wider inline-block w-fit opacity-50">
+        {subTitle}
+      </h4>
+    </div>
+  );
+}
