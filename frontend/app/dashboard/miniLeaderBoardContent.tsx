0ed2f369032082c625694437082d456cbd438914
ina06 <ina06@users.noreply.github.com>
2023-11-21 19:17:02 +0100

    [#26] - adjust margins and paddings for mini leaderboards

--- frontend/app/dashboard/miniLeaderBoardContent.tsx
+++ frontend/app/dashboard/miniLeaderBoardContent.tsx
@@ -10,24 +10,26 @@ export default function MiniLeaderBoardContent({
   entries,
 }: Props): JSX.Element {
   return (
-    <div>
-      <h2 className="text-4xl font-bold mb-4 mx-2.5">{title}</h2>
-      {entries.map((entry, index) => (
-        <span key={index} className="mb-4 p-4 grid grid-cols-5">
-          <p className="font-bold col-span-2 truncate">
-            {entry.place}. {entry.botName}
-          </p>
-          <div className="col-span-2 pr-4">
-            <div className="bg-background h-4 w-full h-full ml-2flex items-center justify-center p-1">
-              <div
-                className="bg-primary_highlight h-full"
-                style={{ width: `${entry.score}%` }}
-              />
+    <div className="pb-4 px-4">
+      <h2 className="text-4xl font-bold mb-4">{title}</h2>
+      <div className="flex flex-col space-y-4">
+        {entries.map((entry, index) => (
+          <div key={index} className="grid grid-cols-5">
+            <p className="font-bold col-span-2 truncate pr-2">
+              {entry.place}. {entry.botName}
+            </p>
+            <div className="col-span-2 pr-4">
+              <div className="bg-background h-4 w-full h-full ml-2flex items-center justify-center p-1">
+                <div
+                  className="bg-primary_highlight h-full"
+                  style={{ width: `${entry.score}%` }}
+                />
+              </div>
             </div>
+            <p className="ml-2 col-span-1 pl-1">Score: {entry.score}</p>
           </div>
-          <p className="ml-2 col-span-1 pl-1">Score: {entry.score}</p>
-        </span>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
