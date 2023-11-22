134360c16da78cbcf9434039d25e2dcec45b78e6
Johannes <jonny.bliha@gmail.com>
2023-11-22 08:40:13 +0100

    [#31] Add Fibonacci Test

--- cardscheme/src/test/kotlin/CompoundTests.kt
+++ cardscheme/src/test/kotlin/CompoundTests.kt
@@ -30,4 +30,51 @@ class CompoundTests {
         SchemeInterpreter().run(program)
         Assert.assertEquals("123", outputStreamCapture.toString().trim())
     }
+
+    @Test
+    fun fibonacciTest() {
+        val interpreter = SchemeInterpreter()
+
+        val program =
+            """
+            (define fib (lambda n
+                (if (< n 3)
+                    1
+                    (+ (fib (- n 1)) (fib (- n 2)))
+            )))
+            """.trimIndent()
+        interpreter.run(program)
+
+        var result = interpreter.run("(fib 1)")
+        assert(result is IntegerValue)
+        Assert.assertEquals(1, (result as IntegerValue).value)
+
+        result = interpreter.run("(fib 2)")
+        assert(result is IntegerValue)
+        Assert.assertEquals(1, (result as IntegerValue).value)
+
+        result = interpreter.run("(fib 3)")
+        assert(result is IntegerValue)
+        Assert.assertEquals(2, (result as IntegerValue).value)
+
+        result = interpreter.run("(fib 4)")
+        assert(result is IntegerValue)
+        Assert.assertEquals(3, (result as IntegerValue).value)
+
+        result = interpreter.run("(fib 5)")
+        assert(result is IntegerValue)
+        Assert.assertEquals(5, (result as IntegerValue).value)
+
+        result = interpreter.run("(fib 6)")
+        assert(result is IntegerValue)
+        Assert.assertEquals(8, (result as IntegerValue).value)
+
+        result = interpreter.run("(fib 7)")
+        assert(result is IntegerValue)
+        Assert.assertEquals(13, (result as IntegerValue).value)
+
+        result = interpreter.run("(fib 8)")
+        assert(result is IntegerValue)
+        Assert.assertEquals(21, (result as IntegerValue).value)
+    }
 }
