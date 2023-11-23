54e386223a4efc3ad2b9ae9c52e03503853f54bf
UnHold (Alex) <alex@hold.re>
2023-11-23 11:02:40 +0100

    [#14] - changed wildcard import to explicit import

--- management/src/main/kotlin/at/tuwien/ase/cardlabs/management/database/model/AccountDAO.kt
+++ management/src/main/kotlin/at/tuwien/ase/cardlabs/management/database/model/AccountDAO.kt
@@ -1,6 +1,13 @@
 package at.tuwien.ase.cardlabs.management.database.model
 
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
 
 @Entity
 @Table(name = "account")
