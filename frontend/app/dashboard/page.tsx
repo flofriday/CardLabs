9d8be33fea17a01b94b07af828eb37a1e9143024
ina06 <ina06@users.noreply.github.com>
2023-11-24 23:26:35 +0100

    [#26] - add links to buttons on dashboard page and adjust button sizing

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import LeftPageHeader from "../components/leftPageHeader";
 import Robot, { RobotType } from "../components/robot";
 import MiniLeaderBoard from "./miniLeaderBoard";
+import Link from "next/link";
 
 export default function Dashboard(): JSX.Element {
   return (
@@ -9,19 +10,31 @@ export default function Dashboard(): JSX.Element {
 
       <Robot type={RobotType.QUESTIONMARK} />
 
-      <div className="flex flex-col justify-center items-center space-y-20 pt-10 w-full h-full">
-        <button className="btn bg-primary  h-24 py-2 w-1/4 font-bold rounded-lg shadow-md text-4xl hover:bg-primary_highlight">
+      <div className="flex flex-col justify-center items-center space-y-16 pt-10 w-full h-full">
+        <Link
+          href="/" // TODO add link to new bot page here
+          className="btn bg-primary flex justify-center items-center h-20 py-2 w-1/4 font-bold rounded-lg shadow-md text-4xl hover:bg-primary_highlight"
+        >
           New Bot
-        </button>
-        <button className="btn bg-primary  h-24 py-2 w-1/4 font-bold rounded-lg shadow-md text-4xl hover:bg-primary_highlight">
+        </Link>
+        <Link
+          href="/" // TODO add link to my bots page here
+          className="btn bg-primary flex justify-center items-center h-20 py-2 w-1/4 font-bold rounded-lg shadow-md text-4xl hover:bg-primary_highlight"
+        >
           My Bots
-        </button>
-        <button className="btn bg-primary  h-24 py-2 w-1/4 font-bold rounded-lg shadow-md text-4xl hover:bg-primary_highlight">
+        </Link>
+        <Link
+          href="/rules"
+          className="btn bg-primary flex justify-center items-center h-20 py-2 w-1/4 font-bold rounded-lg shadow-md text-4xl hover:bg-primary_highlight"
+        >
           Game Rules
-        </button>
-        <button className="btn bg-primary  h-24 py-2 w-1/4 font-bold rounded-lg shadow-md text-4xl hover:bg-primary_highlight">
+        </Link>
+        <Link
+          href="/help"
+          className="btn bg-primary flex justify-center items-center h-20 py-2 w-1/4 font-bold rounded-lg shadow-md text-4xl hover:bg-primary_highlight"
+        >
           Help
-        </button>
+        </Link>
       </div>
 
       <div className="absolute top-0 right-0 space-y-5 flex flex-col justify-center items-center h-full w-1/4 mr-12">
