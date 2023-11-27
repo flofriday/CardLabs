6c92c1aa2a3c7a13a178e5e3c313c0a637892037
Johannes <jonny.bliha@gmail.com>
2023-11-27 17:54:31 +0100

    [#31] Fix ktlint errors

--- cardscheme/src/main/kotlin/SchemeError.kt
+++ cardscheme/src/main/kotlin/SchemeError.kt
@@ -1,42 +1,42 @@
 data class SchemeError(val header: String, val reason: String, val location: Location?, val tip: String?) :
     Throwable() {
     fun display(program: String) {
-        val RESET = "\u001b[0m"
-        val RED = "\u001b[31m"
-        val GREEN = "\u001b[32m"
-        val BLUE = "\u001b[34m"
-        val CYAN = "\u001b[36m"
-        val GREY = "\u001b[37m"
+        val reset = "\u001b[0m"
+        val red = "\u001b[31m"
+        val green = "\u001b[32m"
+        val blue = "\u001b[34m"
+        val cyan = "\u001b[36m"
+        val grey = "\u001b[37m"
 
-        print(CYAN)
+        print(cyan)
         println("-- " + header.uppercase() + " " + "-".repeat(80 - 2 - header.length))
-        println(RESET)
+        println(reset)
         println(reason)
         println()
         if (location != null) {
             if (location.startline == location.endline) {
                 val line = program.lines().get(location.startline - 1)
-                print(GREY)
+                print(grey)
                 print("%03d| ".format(location.startline) + line)
-                println(RESET)
-                println(" ".repeat(5 + location.startcol - 1) + RED + "^".repeat(location.endcol + 1 - location.startcol) + RESET)
+                println(reset)
+                println(" ".repeat(5 + location.startcol - 1) + red + "^".repeat(location.endcol + 1 - location.startcol) + reset)
             } else {
-                print(GREY)
+                print(grey)
                 println(
                     program.lines()
                         .drop(location.startline - 1)
                         .take(location.endline - location.startline + 1)
                         .zip(location.startline..location.endline)
-                        .joinToString("\n") { (l, n) -> "%03d| ".format(n) + RED + "> " + GREY + l },
+                        .joinToString("\n") { (l, n) -> "%03d| ".format(n) + red + "> " + grey + l },
                 )
-                print(RESET)
+                print(reset)
             }
         } else {
             println("    No preview available")
         }
         println()
         if (tip != null) {
-            println(GREEN + "TIP: " + RESET + tip)
+            println(green + "TIP: " + reset + tip)
         }
     }
 }
--- cardscheme/src/main/kotlin/SchemeValue.kt
+++ cardscheme/src/main/kotlin/SchemeValue.kt
@@ -1,4 +1,4 @@
-import java.util.*
+import java.util.LinkedList
 import kotlin.math.absoluteValue
 
 abstract class SchemeValue {
