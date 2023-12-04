e7d70e95c5ae5c01011cab1e8a8597f0c495c628
ina06 <ina06@users.noreply.github.com>
2023-12-04 15:13:21 +0100

    [#22] - add round log container to game detail view and make the container scrollable

--- frontend/app/gameDetail/page.tsx
+++ frontend/app/gameDetail/page.tsx
@@ -14,11 +14,27 @@ export default function GameDetail({ date, place }: Props): JSX.Element {
     { message: "More normal logs." },
     { message: "Debug log with additional information.", isDebug: true },
     { message: "More normal logs." },
+    { message: "This is a normal log message." },
+    { message: "Another log entry." },
+    { message: "Debugging information.", isDebug: true },
+    { message: "More normal logs." },
+    { message: "Debug log with additional information.", isDebug: true },
+    { message: "More normal logs." },
+    { message: "This is a normal log message." },
+    { message: "Another log entry." },
+    { message: "Debugging information.", isDebug: true },
+    { message: "More normal logs." },
+    { message: "Debug log with additional information.", isDebug: true },
+    { message: "More normal logs." },
   ];
 
   return (
     <div className="flex flex-col h-full">
       <LeftPageHeader title="Game XX.YY.ZZZZ - X. Place" />
+
+      <div className="flex flex-1 w-full justify-center items-center pt-32">
+        <RoundLogContainer logLines={logExamples} />
+      </div>
     </div>
   );
 }
--- frontend/app/gameDetail/roundLogContainer.tsx
+++ frontend/app/gameDetail/roundLogContainer.tsx
@@ -11,14 +11,12 @@ export default function RoundLogContainer({ logLines }: Props): JSX.Element {
   let nonDebugIndex = 0;
 
   return (
-    <div className="bg-secondary p-4 text-text rounded-lg overflow-y-auto">
+    <div className="bg-secondary p-4 text-text rounded-lg overflow-y-auto max-h-[65vh]">
       <h2 className="text-4xl font-bold mb-4">Log: </h2>
       {logLines.map((log, index) => (
         <div
           key={index}
-          className={`mb-2 ${
-            log.isDebug ? "text-yellow" : ""
-          } p-2 rounded-md font-bold`}
+          className={`mb-2 ${log.isDebug ? "text-yellow" : ""} p-2 rounded-md`}
         >
           {log.isDebug && "[DBG] "} {!log.isDebug && `${++nonDebugIndex}.`}{" "}
           {log.message}
