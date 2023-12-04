721eec8cf5e9558cb614f1243a4f52e46e544ef5
UnHold (Alex) <alex@hold.re>
2023-12-04 18:08:10 +0100

    [#17] - added modal for delete

--- frontend/app/bot/editor/editorButtons.tsx
+++ frontend/app/bot/editor/editorButtons.tsx
@@ -1,5 +1,7 @@
 import IconButton, { ButtonIcon } from "../../components/iconButton";
 import DropDown from "@/app/components/DropDown";
+import Modal from "@/app/components/modal";
+import { useState } from "react";
 
 interface Props {
   save: () => void;
@@ -7,8 +9,39 @@ interface Props {
 }
 
 export default function EditorButtons({ save, _delete }: Props): JSX.Element {
+  const [deleteModalVisability, setDeleteModalVisability] = useState(false);
+
   return (
     <div className=" w-full mt-44 h-16 flex p-3 space-x-4 ml-4">
+      {deleteModalVisability && (
+        <Modal
+          title="Delete Bot"
+          modalClassName="w-3/12"
+          onClose={() => {
+            setDeleteModalVisability(false);
+          }}
+        >
+          <div>
+            Do you really want to delete your bot?
+            <div className="flex justify-around pt-4">
+              <button
+                className="btn bg-accent text-text py-2 w-48 rounded-lg shadow-md text-lg"
+                onClick={_delete}
+              >
+                Delete Bot
+              </button>
+              <button
+                className="btn bg-secondary text-text py-2 w-48 rounded-lg shadow-md text-lg"
+                onClick={() => {
+                  setDeleteModalVisability(false);
+                }}
+              >
+                Go back
+              </button>
+            </div>
+          </div>
+        </Modal>
+      )}
       <IconButton text="Compile" type={ButtonIcon.COMPILE} />
       <IconButton onClick={save} text="Save" type={ButtonIcon.SAVE} />
       <IconButton text="Test Bot" type={ButtonIcon.TEST} />
@@ -19,7 +52,13 @@ export default function EditorButtons({ save, _delete }: Props): JSX.Element {
         customButtonClass="text-text p-2 w-full h-full rounded-full shadow-md text-lg rounded-full outline outline-1  hover:bg-primary"
         values={[]}
       />
-      <IconButton onClick={_delete} text="Delete" type={ButtonIcon.DELETE} />
+      <IconButton
+        onClick={() => {
+          setDeleteModalVisability(true);
+        }}
+        text="Delete"
+        type={ButtonIcon.DELETE}
+      />
     </div>
   );
 }
