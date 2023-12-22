8f12e2f33fff5d33f93fd9b0ab26a80bf6a8d883
flofriday <flohacksfriday@gmail.com>
2023-12-22 18:22:10 +0100

    [#30] Update colors to be symbols in the botAPI

--- gameserver/src/main/kotlin/simulation/CardMapper.kt
+++ gameserver/src/main/kotlin/simulation/CardMapper.kt
@@ -2,6 +2,7 @@ package simulation
 
 import cardscheme.SchemeValue
 import cardscheme.StringValue
+import cardscheme.SymbolValue
 import cardscheme.VectorValue
 
 fun encodeCard(card: Card): SchemeValue {
@@ -15,7 +16,7 @@ fun encodeCard(card: Card): SchemeValue {
             is ChooseDrawCard -> "chooseDraw"
             else -> throw Exception("Invalid case")
         }
-    return VectorValue(mutableListOf<SchemeValue>(StringValue(card.color.toString().lowercase()), StringValue(type)))
+    return VectorValue(mutableListOf<SchemeValue>(SymbolValue(card.color.toString().lowercase()), StringValue(type)))
 }
 
 fun decodeCard(value: SchemeValue): Card {
@@ -27,11 +28,15 @@ fun decodeCard(value: SchemeValue): Card {
         throw DecodeError("The cards only have 2 members but the value has ${value.values.size}")
     }
 
-    if (!value.values.all { m -> m is StringValue }) {
-        throw DecodeError("Both fields of a card must be strings")
+    if (value.values[0] !is SymbolValue) {
+        throw DecodeError("The first field of a card must be a symbol")
     }
 
-    val color = (value.values[0] as StringValue).value.uppercase()
+    if (value.values[1] !is StringValue) {
+        throw DecodeError("The second field of a card must be a string")
+    }
+
+    val color = (value.values[0] as SymbolValue).value.uppercase()
     val type = (value.values[1] as StringValue).value
     return when {
         type == "switch" -> SwitchCard(Color.valueOf(color))
