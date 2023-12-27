4316b7abd39ee87e7944d1fa906cece5e5d3ca28
flofriday <flohacksfriday@gmail.com>
2023-12-27 17:37:46 +0100

    [#30] Add advanced matrix multiplication testcase

--- gameserver/src/test/kotlin/cardscheme/AdvancedTests.kt
+++ gameserver/src/test/kotlin/cardscheme/AdvancedTests.kt
@@ -419,4 +419,117 @@ No more bottles of beer on the wall, no more bottles of beer.""",
         assert(result is BooleanValue)
         Assert.assertEquals(true, (result as BooleanValue).value)
     }
+
+    /**
+     * Example from the Scheme Cookbook:
+     * https://cookbook.scheme.org/use-lists-as-2d-matrix/
+     */
+    @Test
+    fun matrixMultiplication() {
+        val program = """
+        (define (make-matrix n)
+          (let outter ((i n) (result '()))
+            (if (= i 0)
+                result
+                (outter (- i 1)
+                        (cons
+                         (let inner ((j n) (row '()))
+                           (if (= j 0)
+                               row
+                               (inner (- j 1) (cons (if (= i j) 1 0) row))))
+                         result)))))
+
+        (define (nth list n)
+          (let loop ((n n) (result list))
+            (if (= n 0)
+                (car result)
+                (loop (- n 1)
+                      (cdr result)))))
+
+        (define matrix-row nth)
+
+        (define (matrix-col M n)
+          (let loop ((i (length M)) (result '()))
+            (if (= i 0)
+                result
+                (loop (- i 1)
+                      (cons (nth (nth M (- i 1)) n) result)))))
+
+        (define (matrix-mul N M)
+          (let outter ((i (length N)) (result '()))
+            (if (= i 0)
+                result
+                (outter (- i 1)
+                        (cons
+                         (let inner ((j (length (car M))) (row '()))
+                           (if (= j 0)
+                               row
+                               (inner
+                                (- j 1)
+                                (cons (reduce + (map *
+                                                     (matrix-row N (- i 1))
+                                                     (matrix-col M (- j 1))))
+                                      row))))
+                       result)))))
+
+        (define (reduce fun lst)
+          (let loop ((result (car lst)) (lst (cdr lst)))
+            (if (null? lst)
+                result
+                (loop (fun result (car lst)) (cdr lst)))))
+
+        (define (matrix-vector-mul v M)
+          (car (matrix-mul (list v) M)))
+
+        (define (matrix-transpose M)
+          (let loop ((M M) (result '()))
+            (if (null? (car M))
+                result
+                (loop (map cdr M) (append result (list (map car M)))))))
+
+        (define (matrix-sum N M)
+          (map (lambda (nrow mrow) (map + nrow mrow)) N M))        
+          
+        (define M1 '((1 2 3) (2 3 4) (3 2 1)))
+        (define M2 (make-matrix 3))
+        """.trimIndent()
+        val interpreter = SchemeInterpreter()
+        interpreter.run(program)
+
+        var result = interpreter.run("(matrix-mul M1 M2)")
+        assert(result is ListValue)
+        Assert.assertEquals(
+            ListValue(
+                ListValue(listOf(1, 2, 3).map { i -> IntegerValue(i) }),
+                ListValue(listOf(2, 3, 4).map { i -> IntegerValue(i) }),
+                ListValue(listOf(3, 2, 1).map { i -> IntegerValue(i) }),
+            ), (result as ListValue)
+        )
+
+        result = interpreter.run("(matrix-mul M1 '((2 3 1) (1 2 1) (1 3 1)))")
+        assert(result is ListValue)
+        Assert.assertEquals(
+            ListValue(
+                ListValue(listOf(7, 16, 6).map { i -> IntegerValue(i) }),
+                ListValue(listOf(11, 24, 9).map { i -> IntegerValue(i) }),
+                ListValue(listOf(9, 16, 6).map { i -> IntegerValue(i) }),
+            ), (result as ListValue)
+        )
+
+        result = interpreter.run("(matrix-sum M1 M2)")
+        assert(result is ListValue)
+        Assert.assertEquals(
+            ListValue(
+                ListValue(listOf(2, 2, 3).map { i -> IntegerValue(i) }),
+                ListValue(listOf(2, 4, 4).map { i -> IntegerValue(i) }),
+                ListValue(listOf(3, 2, 2).map { i -> IntegerValue(i) }),
+            ), (result as ListValue)
+        )
+
+        result = interpreter.run("(matrix-vector-mul '(2 3 1) M1)")
+        assert(result is ListValue)
+        Assert.assertEquals(
+            ListValue(listOf(11, 15, 19).map { i -> IntegerValue(i) }), (result as ListValue)
+        )
+    }
 }
