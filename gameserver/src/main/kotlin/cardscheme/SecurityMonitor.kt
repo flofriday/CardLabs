e93a457d0e2abc7786cfc2300e7d5983d24d88ff
Johannes <jonny.bliha@gmail.com>
2023-12-28 14:25:31 +0100

    [#51] Implement timeout limit inside of interpreter

---
+++ gameserver/src/main/kotlin/cardscheme/SecurityMonitor.kt
@@ -0,0 +1,20 @@
+package cardscheme
+
+class TimeoutMonitor(private val instructionLimit: Long) {
+    private var instructionCount = 0
+    fun step(node: AstNode) {
+        instructionCount++
+        checkForTimeout()
+    }
+
+    private fun checkForTimeout() {
+        if (instructionCount >= instructionLimit) {
+            throw SchemeError(
+                "Function exceeded timeout",
+                "The execution of the Interpreter was cancelled because it exceeded the timeout limit of $instructionLimit instructions",
+                null,
+                null,
+            )
+        }
+    }
+}
