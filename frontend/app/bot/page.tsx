c7c0d4ad0956f6aa939dc90aece46abfdf5bafcd
UnHold (Alex) <alex@hold.re>
2024-01-02 19:50:42 +0100

    [#15] - fixed scaling

--- frontend/app/bot/page.tsx
+++ frontend/app/bot/page.tsx
@@ -39,7 +39,7 @@ export default function BotOverview(): JSX.Element {
   return (
     <>
       <LeftPageHeader title="My Bots" margin={5} />
-      <div className="h-full w-full flex items-center justify-center flex-col">
+      <div className=" w-full flex items-center justify-center flex-col mt-28">
         <div className="mt-14 w-9/12 grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-8">
           {bots.map((b, idx) => {
             return <BotCard key={idx} bot={b} refetch={fetch} />;
