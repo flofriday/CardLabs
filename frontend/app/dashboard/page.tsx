5441e23d6c2fc8588368b217392b5927161aa7b4
ina06 <ina06@users.noreply.github.com>
2024-01-02 15:48:28 +0100

    [#22] - Fix display error in dashboard

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -19,35 +19,35 @@ export default function Dashboard(): JSX.Element {
           <Robot type={RobotType.BASIC} />
         </div>
 
-      <div className="flex flex-col justify-center items-center space-y-16 pt-10 w-full h-full">
-        <Link
-          href="/bot/editor"
-          className="btn bg-primary flex justify-center items-center h-20 py-2 w-1/4 font-bold rounded-lg shadow-md text-4xl hover:bg-primary_highlight"
-          id="button_create_new_bot"
-        >
-          New Bot
-        </Link>
-        <Link
-          href="/" // TODO add link to my bots page here
-          className="btn bg-primary flex justify-center items-center h-20 py-2 w-1/4 font-bold rounded-lg shadow-md text-4xl hover:bg-primary_highlight"
-        >
-          My Bots
-        </Link>
-        <Link
-          href="/rules"
-          className="btn bg-primary flex justify-center items-center h-20 py-2 w-1/4 font-bold rounded-lg shadow-md text-4xl hover:bg-primary_highlight"
-        >
-          Game Rules
-        </Link>
-        <Link
-          href="/help"
-          className="btn bg-primary flex justify-center items-center h-20 py-2 w-1/4 font-bold rounded-lg shadow-md text-4xl hover:bg-primary_highlight"
-        >
-          Help
-        </Link>
-      </div>
+        <div className="flex flex-col justify-center items-center space-y-16 pt-10 px-20 w-1/2 h-full">
+          <Link
+            href="/bot/editor"
+            className="btn bg-primary flex justify-center items-center h-20 py-2 w-full font-bold rounded-lg shadow-md text-4xl hover:bg-primary_highlight"
+            id="button_create_new_bot"
+          >
+            New Bot
+          </Link>
+          <Link
+            href="/" // TODO add link to my bots page here
+            className="btn bg-primary flex justify-center items-center w-full h-20 py-2 font-bold rounded-lg shadow-md text-4xl hover:bg-primary_highlight"
+          >
+            My Bots
+          </Link>
+          <Link
+            href="/rules"
+            className="btn bg-primary flex justify-center items-center w-full h-20 py-2 font-bold rounded-lg shadow-md text-4xl hover:bg-primary_highlight"
+          >
+            Game Rules
+          </Link>
+          <Link
+            href="/help"
+            className="btn bg-primary flex justify-center items-center w-full h-20 py-2 font-bold rounded-lg shadow-md text-4xl hover:bg-primary_highlight"
+          >
+            Help
+          </Link>
+        </div>
 
-        <div className="w-2/6 pl-12">
+        <div className="w-2/6 px-12">
           <div className="space-y-5 flex flex-col justify-center items-center w-full w-1/4 mr-12">
             <MiniLeaderBoard
               leaderboardType={LeaderBoardType.MY_BOTS}
