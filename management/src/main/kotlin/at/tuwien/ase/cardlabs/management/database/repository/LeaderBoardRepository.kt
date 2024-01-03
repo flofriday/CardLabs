46c9564bb532afdbe22fd3f75d677267b7c08192
ina06 <ina06@users.noreply.github.com>
2024-01-03 16:18:54 +0100

    [#27] - Fix rank calculation so that the places start at 1 instead of 0

--- management/src/main/kotlin/at/tuwien/ase/cardlabs/management/database/repository/LeaderBoardRepository.kt
+++ management/src/main/kotlin/at/tuwien/ase/cardlabs/management/database/repository/LeaderBoardRepository.kt
@@ -17,15 +17,15 @@ import org.springframework.stereotype.Repository
 @Repository
 interface LeaderBoardRepository : PagingAndSortingRepository<BotDAO?, Long> {
 
-    @Query("""SELECT new at.tuwien.ase.cardlabs.management.controller.model.LeaderBoardEntry((SELECT count(*) FROM BotDAO c WHERE c.eloScore > b.eloScore ), b.eloScore, b.name, b.owner.username)
+    @Query("""SELECT new at.tuwien.ase.cardlabs.management.controller.model.LeaderBoardEntry((SELECT count(*) + 1 FROM BotDAO c WHERE c.eloScore > b.eloScore ), b.eloScore, b.name, b.owner.username)
             FROM BotDAO b ORDER BY b.eloScore DESC""")
     fun getLeaderBoardEntriesGlobal(pageable: Pageable): Page<LeaderBoardEntry>
 
-    @Query("""SELECT new at.tuwien.ase.cardlabs.management.controller.model.LeaderBoardEntry((SELECT count(*) FROM BotDAO c LEFT JOIN c.owner o1 LEFT JOIN o1.location l1 WHERE c.eloScore > b.eloScore AND  l1.continent = :continent), b.eloScore, b.name, b.owner.username)
+    @Query("""SELECT new at.tuwien.ase.cardlabs.management.controller.model.LeaderBoardEntry((SELECT count(*) + 1 FROM BotDAO c LEFT JOIN c.owner o1 LEFT JOIN o1.location l1 WHERE c.eloScore > b.eloScore AND  l1.continent = :continent), b.eloScore, b.name, b.owner.username)
             FROM BotDAO b LEFT JOIN b.owner o2 LEFT JOIN o2.location l2 WHERE l2.continent = :continent ORDER BY b.eloScore DESC""")
     fun getLeaderBoardEntriesContinent(@Param("continent") continent: Continent, pageable: Pageable): Page<LeaderBoardEntry>
 
-    @Query("""SELECT new at.tuwien.ase.cardlabs.management.controller.model.LeaderBoardEntry((SELECT count(*) FROM BotDAO c LEFT JOIN c.owner o1 LEFT JOIN o1.location l1 WHERE c.eloScore > b.eloScore AND  l1.name = :country), b.eloScore, b.name, b.owner.username)
+    @Query("""SELECT new at.tuwien.ase.cardlabs.management.controller.model.LeaderBoardEntry((SELECT count(*) + 1 FROM BotDAO c LEFT JOIN c.owner o1 LEFT JOIN o1.location l1 WHERE c.eloScore > b.eloScore AND  l1.name = :country), b.eloScore, b.name, b.owner.username)
             FROM BotDAO b LEFT JOIN b.owner o2 LEFT JOIN o2.location l2 WHERE l2.name = :country ORDER BY b.eloScore DESC""")
     fun getLeaderBoardEntriesCountry(@Param("country") country: String, pageable: Pageable): Page<LeaderBoardEntry>
 
