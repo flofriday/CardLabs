bd42fa44ad040f518cb89e3c6772762a9abb9f0d
ina06 <ina06@users.noreply.github.com>
2024-01-03 22:30:17 +0100

    [#26] - Remove mock artefacts

--- frontend/app/services/LeaderBoardService.tsx
+++ frontend/app/services/LeaderBoardService.tsx
@@ -6,32 +6,6 @@ import { leaderBoardEntry } from "../types/leaderBoardEntry";
 import { getUserInfo } from "./UserService";
 import { getCookie } from "cookies-next";
 
-// TODO: Remove this as this is just mocked data to test the frontend
-const exampleEntries: leaderBoardEntry[] = [
-  { place: 1, score: 90, botName: "Lewisbot", userName: "User1" },
-  { place: 2, score: 85, botName: "Goropogo", userName: "User2" },
-  { place: 3, score: 80, botName: "Greenpop", userName: "User3" },
-  { place: 4, score: 75, botName: "ThisIsAnAwesome name", userName: "User4" },
-  { place: 5, score: 70, botName: "Bot5", userName: "User5" },
-];
-
-const exampleEntries1: leaderBoardEntry[] = [
-  { place: 1, score: 95, botName: "BotA", userName: "UserA" },
-  { place: 2, score: 88, botName: "BotB", userName: "UserB" },
-  { place: 3, score: 82, botName: "BotC", userName: "UserC" },
-  { place: 4, score: 76, botName: "BotD", userName: "UserD" },
-  { place: 5, score: 70, botName: "BotE", userName: "UserE" },
-];
-
-// Example Entry List 2
-const exampleEntries2: leaderBoardEntry[] = [
-  { place: 1, score: 89, botName: "BotX", userName: "UserX" },
-  { place: 2, score: 83, botName: "BotY", userName: "UserY" },
-  { place: 3, score: 77, botName: "BotZ", userName: "UserZ" },
-  { place: 4, score: 71, botName: "BotW", userName: "UserW" },
-  { place: 5, score: 65, botName: "BotV", userName: "UserV" },
-];
-
 export async function getGlobalTop5LeaderBoardEntries(
   regionType: RegionType
 ): Promise<leaderBoardEntry[]> {
