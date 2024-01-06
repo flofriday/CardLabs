0e9438965786ba7e1b350bf8d094d8700e1a3fba
Johannes <jonny.bliha@gmail.com>
2024-01-06 10:44:34 +0100

    [#38] Clean up code and use map to display errors but render markdown content that did not fail

--- frontend/app/help/page.tsx
+++ frontend/app/help/page.tsx
@@ -5,64 +5,59 @@ import TextItem from "../components/textItem";
 import Robot, { RobotType } from "../components/robot";
 import Markdown from "react-markdown";
 import React, { useState, useEffect } from "react";
+import { toast } from "react-toastify";
 
 export default function Help(): JSX.Element {
-  const [programmingLanguageContent, setProgrammingLanguageContent] =
-    useState("");
-  const [codeTemplateContent, setCodeTemplateContent] = useState("");
-  const [convenienceFunctionsContent, setConvenienceFunctionsContent] =
-    useState("");
-  const [debuggingContent, setDebuggingContent] = useState("");
-  const [botTestingContent, setBotTestingContent] = useState("");
-  const [createNewBotContent, setCreateNewBotContent] = useState("");
-  const [editBotContent, setEditBotContent] = useState("");
+  const [markdownContent, setMarkdownContent] = useState(new Map());
 
   const fetchMarkdown = async (url: string): Promise<string> => {
     try {
       const response = await fetch(url);
       if (!response.ok) {
-        throw new Error(`Failed to fetch ${url}`);
+        throw new Error(`Failed to load markdown file ${url}`);
       }
       return await response.text();
     } catch (error) {
-      throw new Error(
-        `Error fetching Markdown file (${url}): ${(error as Error).message}`
-      );
+      throw new Error(`Failed to load markdown file: ${url}`);
     }
   };
 
-  const loadAllMarkdownFiles = async (): Promise<void> => {
-    try {
-      setCodeTemplateContent(
-        await fetchMarkdown("./documentation/codeTemplate.md")
-      );
-      setProgrammingLanguageContent(
-        await fetchMarkdown("./documentation/cardScheme.md")
-      );
+  const loadAllMarkdownFiles = async (
+    markdownUrlList: string[]
+  ): Promise<void> => {
+    const errorMessages: string[] = [];
 
-      setConvenienceFunctionsContent(
-        await fetchMarkdown("./documentation/convenienceFunctions.md")
-      );
-      setDebuggingContent(await fetchMarkdown("./documentation/debugging.md"));
-      setBotTestingContent(
-        await fetchMarkdown("./documentation/botTesting.md")
-      );
-      setCreateNewBotContent(
-        await fetchMarkdown("./documentation/createBot.md")
-      );
-      setEditBotContent(await fetchMarkdown("./documentation/editBot.md"));
-    } catch (error) {
+    for (const markdownUrl of markdownUrlList) {
+      try {
+        const content = await fetchMarkdown(markdownUrl);
+        setMarkdownContent((map) => new Map(map.set(markdownUrl, content)));
+      } catch (error) {
+        errorMessages.push((error as Error).message);
+      }
+    }
+
+    if (errorMessages.length != 0) {
       throw new Error(
-        `Error fetching Markdown files: ${(error as Error).message}`
+        `Failed to load markdown files for: ${errorMessages.join(", ")}`
       );
     }
   };
 
   useEffect(() => {
-    loadAllMarkdownFiles().catch((error) => {
-      console.error("Error fetching Markdown files:", error);
+    const markdownUrlList = [
+      "./documentation/codeTemplate.md",
+      "./documentation/cardScheme.md",
+      "./documentation/convenienceFunctions.md",
+      "./documentation/debugging.md",
+      "./documentation/botTesting.md",
+      "./documentation/createBot.md",
+      "./documentation/editBot.md",
+    ];
+    loadAllMarkdownFiles(markdownUrlList).catch((error) => {
+      toast.error((error as Error).message);
+      console.error((error as Error).message);
     });
-  });
+  }, []);
 
   return (
     <div>
@@ -72,37 +67,37 @@ export default function Help(): JSX.Element {
       <div className="flex flex-col justify-center items-center pt-52 space-y-6 pb-11">
         <TextItem title="Code template">
           <Markdown className="prose prose-invert max-w-none">
-            {codeTemplateContent}
+            {markdownContent.get("./documentation/codeTemplate.md")}
           </Markdown>
         </TextItem>
         <TextItem title="Convenience functions">
           <Markdown className="prose prose-invert max-w-none">
-            {convenienceFunctionsContent}
+            {markdownContent.get("./documentation/convenienceFunctions.md")}
           </Markdown>
         </TextItem>
         <TextItem title="Debugging">
           <Markdown className="prose prose-invert max-w-none">
-            {debuggingContent}
+            {markdownContent.get("./documentation/debugging.md")}
           </Markdown>
         </TextItem>
         <TextItem title="Testing your bot">
           <Markdown className="prose prose-invert max-w-none">
-            {botTestingContent}
+            {markdownContent.get("./documentation/botTesting.md")}
           </Markdown>
         </TextItem>
         <TextItem title="Creating a new bot">
           <Markdown className="prose prose-invert max-w-none">
-            {createNewBotContent}
+            {markdownContent.get("./documentation/createBot.md")}
           </Markdown>
         </TextItem>
         <TextItem title="Editing a bot">
           <Markdown className="prose prose-invert max-w-none">
-            {editBotContent}
+            {markdownContent.get("./documentation/editBot.md")}
           </Markdown>
         </TextItem>
         <TextItem title="Programming Language">
           <Markdown className="prose prose-invert max-w-none">
-            {programmingLanguageContent}
+            {markdownContent.get("./documentation/cardScheme.md")}
           </Markdown>
         </TextItem>
       </div>
