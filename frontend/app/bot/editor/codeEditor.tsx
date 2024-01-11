14449621679372cd2fe26a343babf4eaf04ba1e1
Johannes <jonny.bliha@gmail.com>
2024-01-11 13:38:48 +0100

    [#50] Switch from monaco editor to CodeMirror CodeMirror seems to have better support for Scheme indentation

--- frontend/app/bot/editor/codeEditor.tsx
+++ frontend/app/bot/editor/codeEditor.tsx
@@ -1,30 +1,69 @@
-import { Editor } from "@monaco-editor/react";
 import { useEffect, useState } from "react";
+import CodeMirror from "@uiw/react-codemirror";
+import { langs } from "@uiw/codemirror-extensions-langs";
+import { tags as t } from "@lezer/highlight";
+import { createTheme, CreateThemeOptions } from "@uiw/codemirror-themes";
 
 interface Props {
   onChange?: (code: string | undefined | null) => void;
   code: string | null;
 }
 
-function setEditorTheme(monaco: any): void {
-  monaco.editor.defineTheme("card-labs", {
-    base: "vs-dark",
-    inherit: true,
-    rules: [
-      {
-        token: "comment",
-        foreground: "5CBC8E",
-      },
-    ],
-    colors: {
-      "editor.foreground": "#FEF9EC",
-      "editorLineNumber.foreground": "#FEF9EC",
-      "editor.background": "#3C5366",
-      "editor.lineHighlightBorder": "#FEF9EC20",
+const Colors = {
+  background: "#3C5366",
+  foreground: "#edecee",
+  comments: "#79C37B",
+  keywords: "#8487E4",
+};
+
+const cardLabsTheme = createTheme({
+  theme: "dark",
+  settings: {
+    background: Colors.background,
+    foreground: Colors.foreground,
+    caret: "#a277ff",
+    selection: "#5a51898f",
+    selectionMatch: "#5a51898f",
+    gutterBackground: Colors.background,
+    gutterForeground: Colors.foreground,
+    gutterBorder: "transparent",
+    lineHighlight: "#a394f033",
+  },
+  styles: [
+    { tag: t.keyword, color: "#a277ff" },
+    {
+      tag: [t.name, t.deleted, t.character, t.macroName],
+      color: Colors.foreground,
     },
-    fontSize: 40,
-  });
-}
+    { tag: [t.propertyName], color: "#ffca85" },
+    {
+      tag: [t.processingInstruction, t.string, t.inserted, t.special(t.string)],
+      color: Colors.keywords,
+    },
+    { tag: [t.function(t.variableName), t.labelName], color: "#ffca85" },
+    {
+      tag: [t.color, t.constant(t.name), t.standard(t.name)],
+      color: Colors.keywords,
+    },
+    { tag: [t.definition(t.name), t.separator], color: Colors.foreground },
+    { tag: [t.className], color: "#82e2ff" },
+    {
+      tag: [t.number, t.changed, t.annotation, t.modifier, t.self, t.namespace],
+      color: Colors.keywords,
+    },
+    { tag: [t.typeName], color: "#82e2ff" },
+    { tag: [t.operator, t.operatorKeyword], color: "#a277ff" },
+    { tag: [t.url, t.escape, t.regexp, t.link], color: "#61ffca" },
+    { tag: [t.meta, t.comment], color: Colors.comments },
+    { tag: t.strong, fontWeight: "bold" },
+    { tag: t.emphasis, fontStyle: "italic" },
+    { tag: t.link, textDecoration: "underline" },
+    { tag: t.heading, fontWeight: "bold", color: "#a277ff" },
+    { tag: [t.atom, t.bool, t.special(t.variableName)], color: "#edecee" },
+    { tag: t.invalid, color: "#ff6767" },
+    { tag: t.strikethrough, textDecoration: "line-through" },
+  ],
+});
 
 export default function CodeEditor({
   onChange = (c) => {},
@@ -37,20 +76,19 @@ export default function CodeEditor({
   }, [code, onChange]);
 
   return (
-    <div className="w-full h-[calc(100%_-_4rem_-_11rem)] bg-secondary">
-      <Editor
+    <div className="w-full h-[calc(100%_-_4rem_-_11rem)] bg-secondary text-lg">
+      <CodeMirror
         height={"100%"}
-        defaultLanguage="scheme"
-        options={{
-          fontSize: 20,
-          autoIndent: "advanced",
-          formatOnPaste: true,
-          formatOnType: true,
+        basicSetup={{
+          lineNumbers: true,
+          bracketMatching: true,
+          indentOnInput: true,
+          closeBrackets: true,
         }}
-        theme="card-labs"
+        extensions={[langs.scheme()]}
+        theme={cardLabsTheme}
         value={_code ?? undefined}
         onChange={onChange}
-        beforeMount={setEditorTheme}
       />
     </div>
   );
