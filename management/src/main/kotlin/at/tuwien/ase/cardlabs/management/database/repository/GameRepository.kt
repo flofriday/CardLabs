9b624cf92d80412d2aed5fda0e3968b7ea6bd4b6
Yekllurt <Yekllurt@users.noreply.github.com>
2024-01-19 17:33:09 +0100

    [60] add postgresql database support for match operations

--- management/src/main/kotlin/at/tuwien/ase/cardlabs/management/database/repository/GameRepository.kt
+++ management/src/main/kotlin/at/tuwien/ase/cardlabs/management/database/repository/GameRepository.kt
@@ -3,31 +3,17 @@ package at.tuwien.ase.cardlabs.management.database.repository
 import at.tuwien.ase.cardlabs.management.database.model.game.GameDAO
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
-import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.NoRepositoryBean
 import org.springframework.data.repository.query.Param
-import org.springframework.stereotype.Repository
 
 /**
  * Game entities for which the deleted field is not null are viewed as deleted entities
  */
-@Repository
+@NoRepositoryBean
 interface GameRepository : CrudRepository<GameDAO?, Long?> {
 
     fun findByIdAndDeletedIsNull(id: Long): GameDAO?
 
-    @Query(
-        /*"""
-        SELECT *
-            FROM game
-            WHERE :botId = ANY(participating_bots)
-        """,*/
-        """
-            SELECT * 
-                FROM game g
-                WHERE (ARRAY_CONTAINS(g.participating_bots_id, :botId))
-        """,
-        nativeQuery = true
-    )
     fun findAllGamesWithBot(@Param("botId") botId: Long, pageable: Pageable): Page<GameDAO>
 }
---
+++ management/src/main/kotlin/at/tuwien/ase/cardlabs/management/database/repository/GameRepositoryPostgreSQL.kt
@@ -0,0 +1,24 @@
+package at.tuwien.ase.cardlabs.management.database.repository
+
+import at.tuwien.ase.cardlabs.management.database.model.game.GameDAO
+import org.springframework.context.annotation.Profile
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
+
+@Repository
+@Profile("!local")
+interface GameRepositoryPostgreSQL : GameRepository {
+
+    @Query(
+        """
+            SELECT *
+                FROM game
+                WHERE :botId = ANY(participating_bot_ids)
+        """,
+        nativeQuery = true
+    )
+    override fun findAllGamesWithBot(@Param("botId") botId: Long, pageable: Pageable): Page<GameDAO>
+}
