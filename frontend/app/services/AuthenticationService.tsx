e9a0d5d6a8a823e6f580618f12486dfe812e58d9
Johannes <jonny.bliha@gmail.com>
2024-01-19 23:52:56 +0100

    Revert "[#48] Temporary Management connection test"
    This reverts commit 8cf84aff45ab8c66c471e025ca3d4189e32ffce4.

--- frontend/app/services/AuthenticationService.tsx
+++ frontend/app/services/AuthenticationService.tsx
@@ -13,8 +13,7 @@ export async function isAuthenticated(): Promise<boolean> {
 export async function validToken(jwt: string): Promise<boolean> {
   // this needs to change if the backend is located on a different url
   // sadly this can't be "api/authentication", but I dont know why
-  const managementUrl =
-    process.env.MANAGMENT_HOST ?? "http://10.43.225.43:8080";
+  const managementUrl = process.env.MANAGMENT_HOST ?? "http://127.0.0.1:8080";
 
   const response = await fetch(`${managementUrl}/authentication`, {
     mode: "cors",
