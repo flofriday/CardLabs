46d45e4bbca1ee1d502c4902e9c86914e9dbed78
flofriday <flohacksfriday@gmail.com>
2024-01-22 22:26:05 +0100

    [#62] Remove unused code

--- gameserver/src/main/kotlin/cardscheme/SchemeInterpreter.kt
+++ gameserver/src/main/kotlin/cardscheme/SchemeInterpreter.kt
@@ -2,10 +2,6 @@ package cardscheme
 
 class SchemeInterpreter(
     private val outputBuffer: StringBuilder = StringBuilder(),
-    /* private val timeLimitInSeconds: Long = 2,
-    private val memoryLimit: Long = 1024 * 1024 * 1024,
-    private val timeoutBetweenChecks: Long = 100,
-     */
     private val instructionLimit: Long = 100000,
     private val memoryLimit: Long = 1024 * 1024,
 ) {
@@ -48,7 +44,7 @@ class SchemeInterpreter(
     fun run(
         func: CallableValue,
         args: List<SchemeValue>,
-    ): SchemeValue? {
+    ): SchemeValue {
         try {
             return Executor(env, outputBuffer, schemeSecurityMonitor).execute(func, args)
         } catch (e: StackOverflowError) {
@@ -60,48 +56,4 @@ class SchemeInterpreter(
             )
         }
     }
-
-    /*
-    private fun runWithTimeoutAndMemoryLimit(function: () -> SchemeValue?): SchemeValue? {
-        val executor: ExecutorService = Executors.newSingleThreadExecutor()
-        val memoryMonitorThread = Thread(MemoryMonitor(Thread.currentThread(), memoryLimit, timeoutBetweenChecks))
-
-        try {
-            val future: Future<SchemeValue?> =
-                executor.submit(
-                    Callable {
-                        function()
-                    },
-                )
-
-            memoryMonitorThread.start()
-
-            val result = future.get(timeLimitInSeconds, TimeUnit.SECONDS)
-            memoryMonitorThread.interrupt()
-            return result
-        } catch (e: TimeoutException) {
-            memoryMonitorThread.interrupt()
-            throw SchemeError(
-                "Function exceeded timeout",
-                "The execution of the Interpreter was cancelled because it exceeded the timeout of $timeLimitInSeconds seconds",
-                null,
-                null,
-            )
-        } catch (e: InterruptedException) {
-            throw SchemeError(
-                "Function exceeded memory limit",
-                "The execution of the Interpreter was cancelled because it exceeded the memory limit of $memoryLimit",
-                null,
-                null,
-            )
-        } catch (e: ExecutionException) {
-            if (e.cause is SchemeError) {
-                throw e.cause as SchemeError
-            }
-            throw e
-        } finally {
-            executor.shutdownNow()
-        }
-    }
-     */
 }
