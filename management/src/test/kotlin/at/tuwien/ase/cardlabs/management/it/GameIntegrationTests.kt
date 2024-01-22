8bef42094dc15ea78dd461d449c19a33dc098418
UnHold (Alex) <alex@hold.re>
2024-01-22 23:46:17 +0100

    [#22] - fixed bot tests (invalid user bot was given)

--- management/src/test/kotlin/at/tuwien/ase/cardlabs/management/it/GameIntegrationTests.kt
+++ management/src/test/kotlin/at/tuwien/ase/cardlabs/management/it/GameIntegrationTests.kt
@@ -15,7 +15,9 @@ import at.tuwien.ase.cardlabs.management.database.model.game.log.DebugLogMessage
 import at.tuwien.ase.cardlabs.management.database.model.game.log.SystemLogMessage
 import at.tuwien.ase.cardlabs.management.database.model.game.turn.Turn
 import at.tuwien.ase.cardlabs.management.database.repository.GameRepository
+import at.tuwien.ase.cardlabs.management.security.CardLabUser
 import at.tuwien.ase.cardlabs.management.service.AccountService
+import at.tuwien.ase.cardlabs.management.service.bot.BotService
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -34,7 +36,6 @@ import java.time.ZoneOffset
 @AutoConfigureMockMvc
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class GameIntegrationTests {
-
     @Autowired
     private lateinit var objectMapper: ObjectMapper
 
@@ -45,12 +46,21 @@ class GameIntegrationTests {
     private lateinit var accountService: AccountService
 
     @Autowired
+    private lateinit var botService: BotService
+
+    @Autowired
     private lateinit var mockMvc: MockMvc
 
     @Test
     fun whenGameFetchAllById_widthNonExistingGame_expectGameDoesNotExistError() {
         val account = TestHelper.createAccount(accountService)
-        val accessToken = TestHelper.getInitialAuthenticationTokens(objectMapper, mockMvc, account.username, TestHelper.DEFAULT_PASSWORD).accessToken
+        val accessToken =
+            TestHelper.getInitialAuthenticationTokens(
+                objectMapper,
+                mockMvc,
+                account.username,
+                TestHelper.DEFAULT_PASSWORD,
+            ).accessToken
         val gameId = 0L
         mockMvc.perform(
             get("/match/$gameId/all")
@@ -63,47 +73,65 @@ class GameIntegrationTests {
     @Test
     fun whenGameFetchAllById_withExistingGame_expectSuccess() {
         val account = TestHelper.createAccount(accountService)
-        val accessToken = TestHelper.getInitialAuthenticationTokens(objectMapper, mockMvc, account.username, TestHelper.DEFAULT_PASSWORD).accessToken
-        val botId = 0L
+        val accessToken =
+            TestHelper.getInitialAuthenticationTokens(
+                objectMapper,
+                mockMvc,
+                account.username,
+                TestHelper.DEFAULT_PASSWORD,
+            ).accessToken
+        val bot =
+            TestHelper.createBot(
+                botService,
+                CardLabUser(account.id!!, "test123", "test@local", "12345"),
+                "Neozoros",
+                "asdf",
+            )
 
         val gameDAO = GameDAO()
         gameDAO.startTime = LocalDateTime.of(2023, 12, 11, 15, 0).toInstant(ZoneOffset.UTC)
         gameDAO.endTime = LocalDateTime.of(2023, 12, 11, 16, 0).toInstant(ZoneOffset.UTC)
-        gameDAO.winningBotId = botId
+        gameDAO.winningBotId = bot.id!!
         val topCard = Card(CardType.DRAW_TWO, Color.CYAN, null)
-        val drawPile = listOf(
-            Card(CardType.DRAW_TWO, Color.CYAN, null),
-            Card(CardType.NUMBER_CARD, Color.ORANGE, 5),
-        )
-        val hand = listOf(
-            Hand(botId, emptyList())
-        )
-        val actions = listOf(
-            Action(
-                botId,
-                ActionType.PLAY_CARD,
-                Card(CardType.DRAW_TWO, Color.ORANGE, null),
+        val drawPile =
+            listOf(
+                Card(CardType.DRAW_TWO, Color.CYAN, null),
+                Card(CardType.NUMBER_CARD, Color.ORANGE, 5),
             )
-        )
-        val logMessages = listOf(
-            SystemLogMessage("hello"),
-            DebugLogMessage("world", botId),
-        )
-        gameDAO.turns = listOf(
-            Turn(0, 0, topCard, drawPile, hand, actions, logMessages),
-        )
+        val hand =
+            listOf(
+                Hand(bot.id!!, emptyList()),
+            )
+        val actions =
+            listOf(
+                Action(
+                    bot.id!!,
+                    ActionType.PLAY_CARD,
+                    Card(CardType.DRAW_TWO, Color.ORANGE, null),
+                ),
+            )
+        val logMessages =
+            listOf(
+                SystemLogMessage("hello"),
+                DebugLogMessage("world", bot.id!!),
+            )
+        gameDAO.turns =
+            listOf(
+                Turn(0, 0, topCard, drawPile, hand, actions, logMessages),
+            )
         gameDAO.gameState = GameState.CREATED
-        gameDAO.participatingBotIds = listOf(botId)
+        gameDAO.participatingBotIds = listOf(bot.id!!)
         var x = gameRepository.save(gameDAO)
         val gameId = x.id!!
 
-        val result = mockMvc.perform(
-            get("/match/$gameId/all")
-                .contentType(MediaType.APPLICATION_JSON)
-                .header("Authorization", "Bearer ${accessToken.token}"),
-        )
-            .andExpect(status().isOk)
-            .andReturn()
+        val result =
+            mockMvc.perform(
+                get("/match/$gameId/all")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .header("Authorization", "Bearer ${accessToken.token}"),
+            )
+                .andExpect(status().isOk)
+                .andReturn()
         val response = objectMapper.readValue<Game>(result.response.contentAsString)
 
         assertEquals(gameDAO.startTime, response.startTime)
@@ -127,7 +155,13 @@ class GameIntegrationTests {
     @Test
     fun whenGameFetchLogById_widthNonExistingGame_expectGameDoesNotExistError() {
         val account = TestHelper.createAccount(accountService)
-        val accessToken = TestHelper.getInitialAuthenticationTokens(objectMapper, mockMvc, account.username, TestHelper.DEFAULT_PASSWORD).accessToken
+        val accessToken =
+            TestHelper.getInitialAuthenticationTokens(
+                objectMapper,
+                mockMvc,
+                account.username,
+                TestHelper.DEFAULT_PASSWORD,
+            ).accessToken
         val gameId = 0L
         mockMvc.perform(
             get("/match/$gameId/all")
@@ -140,32 +174,47 @@ class GameIntegrationTests {
     @Test
     fun whenGameFetchLogById_withExistingGame_expectSuccess() {
         val account = TestHelper.createAccount(accountService)
-        val accessToken = TestHelper.getInitialAuthenticationTokens(objectMapper, mockMvc, account.username, TestHelper.DEFAULT_PASSWORD).accessToken
-        val botId = 0L
+        val accessToken =
+            TestHelper.getInitialAuthenticationTokens(
+                objectMapper,
+                mockMvc,
+                account.username,
+                TestHelper.DEFAULT_PASSWORD,
+            ).accessToken
+        val bot =
+            TestHelper.createBot(
+                botService,
+                CardLabUser(account.id!!, "test123", "test@local", "12345"),
+                "Neozoros",
+                "asdf",
+            )
 
         val gameDAO = GameDAO()
         gameDAO.startTime = LocalDateTime.of(2023, 12, 11, 15, 0).toInstant(ZoneOffset.UTC)
         gameDAO.endTime = LocalDateTime.of(2023, 12, 11, 16, 0).toInstant(ZoneOffset.UTC)
-        gameDAO.winningBotId = botId
+        gameDAO.winningBotId = bot.id!!
         val topCard = Card(CardType.DRAW_TWO, Color.CYAN, null)
-        val logMessages = listOf(
-            SystemLogMessage("hello"),
-            DebugLogMessage("world", botId),
-        )
-        gameDAO.turns = listOf(
-            Turn(0, 0, topCard, emptyList(), emptyList(), emptyList(), logMessages),
-        )
+        val logMessages =
+            listOf(
+                SystemLogMessage("hello"),
+                DebugLogMessage("world", bot.id!!),
+            )
+        gameDAO.turns =
+            listOf(
+                Turn(0, 0, topCard, emptyList(), emptyList(), emptyList(), logMessages),
+            )
         gameDAO.gameState = GameState.CREATED
-        gameDAO.participatingBotIds = listOf(botId)
+        gameDAO.participatingBotIds = listOf(bot.id!!)
         val gameId = gameRepository.save(gameDAO).id!!
 
-        val result = mockMvc.perform(
-            get("/match/$gameId/all")
-                .contentType(MediaType.APPLICATION_JSON)
-                .header("Authorization", "Bearer ${accessToken.token}"),
-        )
-            .andExpect(status().isOk)
-            .andReturn()
+        val result =
+            mockMvc.perform(
+                get("/match/$gameId/all")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .header("Authorization", "Bearer ${accessToken.token}"),
+            )
+                .andExpect(status().isOk)
+                .andReturn()
         val response = objectMapper.readValue<Game>(result.response.contentAsString)
 
         assertEquals(gameDAO.startTime, response.startTime)
