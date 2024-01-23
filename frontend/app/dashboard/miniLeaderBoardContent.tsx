a4ca5468cfb88b158268bd80928526ebf38a932b
ina06 <ina06@users.noreply.github.com>
2024-01-23 16:40:54 +0100

    [#64] - Add information in mini scoreboard if no bots are available yet

--- frontend/app/dashboard/miniLeaderBoardContent.tsx
+++ frontend/app/dashboard/miniLeaderBoardContent.tsx
@@ -15,7 +15,9 @@ export default function MiniLeaderBoardContent({
   regionType,
   entries,
 }: Props): JSX.Element {
-  const [maximalScore, setMaximalScore] = useState<number | undefined>(undefined);
+  const [maximalScore, setMaximalScore] = useState<number | undefined>(
+    undefined
+  );
 
   useEffect(() => {
     getScoreOfGlobalFirstPlace()
@@ -37,26 +39,34 @@ export default function MiniLeaderBoardContent({
   return (
     <div className="pb-4 px-4">
       <h2 className="text-4xl font-bold mb-4">{title}</h2>
-      <div className="flex flex-col space-y-4">
-        {entries.map((entry, index) => (
-          <div key={index} className="grid grid-cols-5">
-            <p className="font-bold col-span-2 truncate pr-2">
-              {entry.place + ". " + entry.botName}
-            </p>
-            <div className="col-span-2 pr-4">
-              <div className="bg-background h-4 w-full h-full ml-2flex items-center justify-center p-1">
-                {maximalScore !== undefined && (
+      {entries.length > 0 ? (
+        <div className="flex flex-col space-y-4">
+          {entries.map((entry, index) => (
+            <div key={index} className="grid grid-cols-5">
+              <p className="font-bold col-span-2 truncate pr-2">
+                {entry.place + ". " + entry.botName}
+              </p>
+              <div className="col-span-2 pr-4">
+                <div className="bg-background h-4 w-full h-full ml-2flex items-center justify-center p-1">
+                  {maximalScore !== undefined && (
                     <div
-                        className="bg-primary_highlight h-full"
-                        style={{ width: `${(entry.score / maximalScore) * 100}%` }}
+                      className="bg-primary_highlight h-full"
+                      style={{
+                        width: `${(entry.score / maximalScore) * 100}%`,
+                      }}
                     />
-                )}
+                  )}
+                </div>
               </div>
+              <p className="ml-2 col-span-1 pl-1">Score: {entry.score}</p>
             </div>
-            <p className="ml-2 col-span-1 pl-1">Score: {entry.score}</p>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      ) : (
+        <div className="flex flex-col space-y-4 font-bold">
+          No scoreboard data available yet!
+        </div>
+      )}
     </div>
   );
 }
