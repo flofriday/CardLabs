329b8d9ebeebdf3e25c608568d508eb89547155b
UnHold (Alex) <alex@hold.re>
2024-01-23 22:40:55 +0100

    [#21] - escaped quote

--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -4,7 +4,7 @@ import { isAuthenticated } from "./services/AuthenticationService";
 export default async function NotFoundPage(): Promise<JSX.Element> {
   return (
     <div className="h-full w-full flex justify-center items-center flex-col space-y-6">
-      <h1 className="text-8xl font-bold">DON'T PANIC</h1>
+      <h1 className="text-8xl font-bold">DON&apos;T PANIC</h1>
       <h1 className="text-8xl font-bold">404</h1>
       <h1 className="text-xl">Just grab your towel, we will guide you back</h1>
       <Link
