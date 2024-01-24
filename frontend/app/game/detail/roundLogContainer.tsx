81a71b9afc59bc70c5ec9f70b7f2de1f5b834de8
flofriday <flohacksfriday@gmail.com>
2024-01-24 15:56:08 +0100

    [#69] Use monospace font to display log messages.
    This especially makes sense because we can now reuse the error printing
    from the interpreter.

--- frontend/app/game/detail/roundLogContainer.tsx
+++ frontend/app/game/detail/roundLogContainer.tsx
@@ -9,14 +9,15 @@ export default function RoundLogContainer({ logLines }: Props): JSX.Element {
     <div className="bg-secondary p-4 text-text rounded-lg overflow-y-auto max-h-[74vh]">
       <h2 className="text-4xl font-bold mb-4">Log: </h2>
       {logLines.map((log, index) => (
-        <div
+        <pre
           key={index}
           className={`mb-2 ${
             log.type !== "system" ? "text-yellow" : ""
           } p-2 rounded-md`}
         >
-          {log.type !== "system" && "[DBG] "} {log.message}
-        </div>
+          {log.type !== "system" && "[DBG] "}
+          {log.message.trim()}
+        </pre>
       ))}
     </div>
   );
